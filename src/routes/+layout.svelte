a9b1032d739cf673e74cd7803edc0be377be0d03
songmingi <alsrl8@naver.com>
2025-07-27 08:11:24 +0900

    chore: remove unused 'fill' attribute from SVG tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,7 @@
 						aria-label="Toggle sidebar"
 					>
 						<!-- Hamburger icon -->
-						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-white"  viewBox="0 0 24 24" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 						</svg>
 					</button>
